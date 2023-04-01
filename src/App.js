d99ee5eb3d488cc43803fdb12cbcbab96264a759
Rohit Nandagawali <nandagawalirohit143@gmail.com>
2023-04-01 20:44:04 +0530

    optimized

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import './App.css'
-import React, { Suspense } from 'react';
-import Console from './Console.jsx'
-
-const Spline = React.lazy(() => import('@splinetool/react-spline'));
+import Spline from '@splinetool/react-spline'
 
 export default function App() {
   return (
-    <div>
-      <Suspense fallback={<Console/>}>
-        <Spline className="spline" scene="https://prod.spline.design/K5N7YUb5ultu502g/scene.splinecode" />
-      </Suspense>
-    </div>
+    <Spline className='spline' scene="https://prod.spline.design/K5N7YUb5ultu502g/scene.splinecode" />
   );
 }
--- src/Console.jsx
+++ src/Console.jsx
@@ -1,10 +1,8 @@
-import './Console.css'
-import Typist from "react-typist";
+import React, { useState, useEffect } from 'react';
+import './Console.css';
 
-export default function Console()  {
-
-
-  const messages = [
+export default function Console(){
+  const loadingMessages= [
     "Loading 3D room...",
     "Installing packages...",
     "Initializing components...",
@@ -26,21 +24,23 @@ export default function Console()  {
     "Initializing application...",
     "Launching program...",
      "Starting, Please Wait..."
-  ];
+
+  ]
+  const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      if (currentMessageIndex < loadingMessages.length - 1) {
+        setCurrentMessageIndex(currentMessageIndex + 1);
+      }
+    }, 2000);
+    return () => clearTimeout(timeoutId);
+  }, [currentMessageIndex, loadingMessages]);
 
   return (
-    <div className="console">
-     
-        <div className="loading">
-          <Typist cursor={{ hideWhenDone: true }} avgTypingDelay={50}>
-            {messages.map((message, index) => (
-              <div key={index}>{message}</div>
-            ))}
-          </Typist>
-          <div className="loader">/</div>
-        </div>
-     
+    <div className="loader">
+      <span className="loading-message">{loadingMessages[currentMessageIndex]}</span>
+      <span className="loading-slash">/</span>
     </div>
   );
 };
-
