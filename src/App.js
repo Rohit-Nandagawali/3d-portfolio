13193eb66f459cbcfd3606027321108e1f831226
Rohit Nandagawali <nandagawalirohit143@gmail.com>
2023-04-01 15:39:57 +0530

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,72 +1,15 @@
 import './App.css'
-import Spline from '@splinetool/react-spline';
-import React, { useState, useEffect } from "react";
-const Loader = ({ progress }) => {
+
+import React, { Suspense } from 'react';
+
+const Spline = React.lazy(() => import('@splinetool/react-spline'));
+
+export default function App() {
   return (
     <div>
-      <h2>Loading...</h2>
-      <div className="progress-bar">
-        <div
-          className="progress-bar-fill"
-          style={{ width: `${progress}%` }}
-        ></div>
-      </div>
-      <p>{progress}%</p>
+      <Suspense fallback={<div>Loading...</div>}>
+      <Spline scene="https://prod.spline.design/K5N7YUb5ultu502g/scene.splinecode" />
+      </Suspense>
     </div>
   );
-};
-
-const App = () => {
-  const [loadingProgress, setLoadingProgress] = useState(0);
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    let intervalId = null;
-
-    const loadSpline = async () => {
-      const startTime = Date.now();
-      await new Promise((resolve) => {
-        const timer = setInterval(() => {
-          const elapsedTime = Date.now() - startTime;
-          const progress = Math.round(
-            Math.min((elapsedTime / 5000) * 100, 100)
-          );
-          setLoadingProgress(progress);
-        }, 100);
-
-        // Load your Spline component here
-        // Once loaded, clear the timer and resolve the promise
-        const spline = (
-          <Spline
-            className="spline"
-            scene="https://prod.spline.design/K5N7YUb5ultu502g/scene.splinecode"
-            onLoad={() => {
-              clearInterval(timer);
-              setIsLoaded(true);
-              resolve();
-            }}
-          />
-        );
-        setIsLoaded(spline);
-      });
-    };
-
-    loadSpline();
-
-    return () => clearInterval(intervalId);
-  }, []);
-
-  return (
-    <div>
-      {!isLoaded ? (
-        <Loader progress={loadingProgress} />
-      ) : (
-        <Spline
-        className="spline"
-        scene="https://prod.spline.design/K5N7YUb5ultu502g/scene.splinecode"/>
-      )}
-    </div>
-  );
-};
-
-export default App;
+}
