63dac7bba43346ab984b34efbde64f9b65de1459
Rohit Nandagawali <nandagawalirohit143@gmail.com>
2023-04-01 14:18:57 +0530

    changes mad

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import './App.css'
+import Spline from '@splinetool/react-spline';
+import React, { useState, useEffect } from "react";
+const Loader = ({ progress }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Loading...</h2>
+      <div className="progress-bar">
+        <div
+          className="progress-bar-fill"
+          style={{ width: `${progress}%` }}
+        ></div>
+      </div>
+      <p>{progress}%</p>
     </div>
   );
-}
+};
+
+const App = () => {
+  const [loadingProgress, setLoadingProgress] = useState(0);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    let intervalId = null;
+
+    const loadSpline = async () => {
+      const startTime = Date.now();
+      await new Promise((resolve) => {
+        const timer = setInterval(() => {
+          const elapsedTime = Date.now() - startTime;
+          const progress = Math.round(
+            Math.min((elapsedTime / 5000) * 100, 100)
+          );
+          setLoadingProgress(progress);
+        }, 100);
+
+        // Load your Spline component here
+        // Once loaded, clear the timer and resolve the promise
+        const spline = (
+          <Spline
+            className="spline"
+            scene="https://prod.spline.design/K5N7YUb5ultu502g/scene.splinecode"
+            onLoad={() => {
+              clearInterval(timer);
+              setIsLoaded(true);
+              resolve();
+            }}
+          />
+        );
+        setIsLoaded(spline);
+      });
+    };
+
+    loadSpline();
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div>
+      {!isLoaded ? (
+        <Loader progress={loadingProgress} />
+      ) : (
+        <p>hello</p>
+      )}
+    </div>
+  );
+};
 
 export default App;
